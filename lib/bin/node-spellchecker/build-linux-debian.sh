21ce33de1cb5bdd4490cd35ae56bee0a48076657
build <ban@necne.pl>
2017-09-20 19:07:52 +0200

    Separate bindings for debian/rpm

---
+++ lib/bin/node-spellchecker/build-linux-debian.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# requires node-gyp (npm install -g node-gyp) plus CLI developper tools
+
+node-gyp rebuild --target=1.7.3 --arch=x64 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-deb-1.7.3-x64.node
+
+node-gyp rebuild --target=1.7.3 --arch=ia32 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-deb-1.7.3-ia32.node
+
+node-gyp rebuild --target=1.7.7 --arch=x64 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-deb-1.7.7-x64.node
+
+node-gyp rebuild --target=1.7.7 --arch=ia32 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-deb-1.7.7-ia32.node
