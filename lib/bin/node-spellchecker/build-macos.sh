ed7efb5eed74c71a3ecfc4ee550e07455829b0bc
Bartosz Antosik <bartosz.antosik@gmail.com>
2018-04-16 12:17:59 +0200

    Updated for version 1.8.4

--- lib/bin/node-spellchecker/build-macos.sh
+++ lib/bin/node-spellchecker/build-macos.sh
@@ -2,11 +2,11 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools
 
-node-gyp rebuild --target=1.7.3 --arch=x64 --dist-url=https://atom.io/download/electron
-cp build/Release/spellchecker.node ../spellchecker-darwin-1.7.3-x64.node
-
 node-gyp rebuild --target=1.7.7 --arch=x64 --dist-url=https://atom.io/download/electron
 cp build/Release/spellchecker.node ../spellchecker-darwin-1.7.7-x64.node
+
+node-gyp rebuild --target=1.8.4 --arch=x64 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-darwin-1.8.4-x64.node
 
 
 
