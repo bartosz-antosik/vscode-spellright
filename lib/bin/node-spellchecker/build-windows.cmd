9810642116bb0dde8a580947de613262cdf54ecd
Bartosz Antosik <bartosz.antosik@gmail.com>
2018-09-13 22:45:56 +0200

    Updated for Electron version 2.0

--- lib/bin/node-spellchecker/build-windows.cmd
+++ lib/bin/node-spellchecker/build-windows.cmd
@@ -9,3 +9,9 @@ copy build\Release\spellchecker.node ..\spellchecker-win32-1.8.4-ia32.node
 
 call node-gyp rebuild --target=1.8.4 --arch=x64 --dist-url=https://atom.io/download/electron
 copy build\Release\spellchecker.node ..\spellchecker-win32-1.8.4-x64.node
+
+call node-gyp rebuild --target=2.0.7 --arch=ia32 --dist-url=https://atom.io/download/electron
+copy build\Release\spellchecker.node ..\spellchecker-win32-2.0.7-ia32.node
+
+call node-gyp rebuild --target=2.0.7 --arch=x64 --dist-url=https://atom.io/download/electron
+copy build\Release\spellchecker.node ..\spellchecker-win32-2.0.7-x64.node
