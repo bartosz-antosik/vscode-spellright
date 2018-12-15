1d5d56906cf26099227a3b8b72f9adb56f5eaa34
Bartosz Antosik <bartosz.antosik@gmail.com>
2018-12-15 22:32:45 +0100

    Binaries update for Electron version 3.x

--- lib/bin/node-spellchecker/build-macos.sh
+++ lib/bin/node-spellchecker/build-macos.sh
@@ -2,11 +2,14 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools
 
-node-gyp rebuild --target=1.7.7 --arch=x64 --dist-url=https://atom.io/download/electron
-cp build/Release/spellchecker.node ../spellchecker-darwin-1.7.7-x64.node
+# node-gyp rebuild --target=1.7.7 --arch=x64 --dist-url=https://atom.io/download/electron
+# cp build/Release/spellchecker.node ../spellchecker-darwin-1.7.7-x64.node
 
-node-gyp rebuild --target=1.8.4 --arch=x64 --dist-url=https://atom.io/download/electron
-cp build/Release/spellchecker.node ../spellchecker-darwin-1.8.4-x64.node
+# node-gyp rebuild --target=1.8.4 --arch=x64 --dist-url=https://atom.io/download/electron
+# cp build/Release/spellchecker.node ../spellchecker-darwin-1.8.4-x64.node
 
+node-gyp rebuild --target=2.0.7 --arch=x64 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-darwin-2.0.7-x64.node
 
-
+node-gyp rebuild --target=3.0.10 --arch=x64 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-darwin-3.0.10-x64.node
