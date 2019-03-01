9f45a8a9e087ebefbd6801bb81969bf4810b6110
Bartosz Antosik <bartosz.antosik@gmail.com>
2019-03-01 22:15:22 +0100

    Language server architecture, initial commit

---
+++ src/server.js
@@ -0,0 +1,126 @@
+// -----------------------------------------------------------------------------
+// Spell Right extension for Visual Studio Code (VSCode)
+// Copyright (c) 2017-2018 Bartosz Antosik. Licensed under the MIT License.
+// -----------------------------------------------------------------------------
+
+'use strict';
+
+const vscode_languageserver_1 = require("vscode-languageserver");
+
+// Create a connection for the server. The connection uses Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+let connection = vscode_languageserver_1.createConnection(vscode_languageserver_1.ProposedFeatures.all);
+
+// Create a simple text document manager. The text document manager
+// supports full document sync only
+let documents = new vscode_languageserver_1.TextDocuments();
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+
+connection.onInitialize((params) => {
+    let capabilities = params.capabilities;
+    // Does the client support the `workspace/configuration` request?
+    // If not, we will fall back using global settings
+    hasConfigurationCapability = !!(capabilities.workspace && !!capabilities.workspace.configuration);
+    hasWorkspaceFolderCapability = !!(capabilities.workspace && !!capabilities.workspace.workspaceFolders);
+    hasDiagnosticRelatedInformationCapability =
+        !!(capabilities.textDocument &&
+            capabilities.textDocument.publishDiagnostics &&
+            capabilities.textDocument.publishDiagnostics.relatedInformation);
+    return {
+        capabilities: {
+            textDocumentSync: documents.syncKind,
+            // Tell the client that the server DOES not supports code completion
+            completionProvider: { resolveProvider: false }
+        }
+    };
+});
+
+connection.onInitialized(() => {
+    if (hasConfigurationCapability) {
+        // Register for all configuration changes.
+        connection.client.register(vscode_languageserver_1.DidChangeConfigurationNotification.type, undefined);
+    }
+    if (hasWorkspaceFolderCapability) {
+        connection.workspace.onDidChangeWorkspaceFolders(_event => {
+            connection.console.log('Workspace folder change event received.');
+        });
+    }
+});
+
+// The global settings, used when the `workspace/configuration` request is not supported by the client.
+// Please note that this is not the case when using this server with the client provided in this example
+// but could happen with other clients.
+const defaultSettings = { maxNumberOfProblems: 1000 };
+
+let globalSettings = defaultSettings;
+
+// Cache the settings of all open documents
+let documentSettings = new Map();
+
+connection.onDidChangeConfiguration(change => {
+    if (hasConfigurationCapability) {
+        // Reset all cached document settings
+        documentSettings.clear();
+    }
+    else {
+        globalSettings = ((change.settings.languageServerExample || defaultSettings));
+    }
+    // Revalidate all open text documents
+    documents.all().forEach(validateTextDocument);
+});
+
+function getDocumentSettings(resource) {
+    if (!hasConfigurationCapability) {
+        return Promise.resolve(globalSettings);
+    }
+    let result = documentSettings.get(resource);
+    if (!result) {
+        result = connection.workspace.getConfiguration({
+            scopeUri: resource,
+            section: 'languageServerExample'
+        });
+        documentSettings.set(resource, result);
+    }
+    return result;
+}
+
+// Only keep settings for open documents
+documents.onDidClose(e => {
+    documentSettings.delete(e.document.uri);
+});
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent(change => {
+
+});
+
+connection.onDidOpenTextDocument((params) => {
+    // A text document got opened in VSCode.
+    // params.uri uniquely identifies the document. For documents store on disk this is a file URI.
+    // params.text the initial full content of the document.
+    connection.console.log(`${params.textDocument.uri} opened.`);
+});
+
+connection.onDidChangeTextDocument((params) => {
+    // The content of a text document did change in VSCode.
+    // params.uri uniquely identifies the document.
+    // params.contentChanges describe the content changes to the document.
+    connection.console.log(`${params.textDocument.uri} changed: ${JSON.stringify(params.contentChanges)}`);
+});
+
+connection.onDidCloseTextDocument((params) => {
+    // A text document got closed in VSCode.
+    // params.uri uniquely identifies the document.
+    connection.console.log(`${params.textDocument.uri} closed.`);
+});
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+// Listen on the connection
+connection.listen();
+//# sourceMappingURL=server.js.map
