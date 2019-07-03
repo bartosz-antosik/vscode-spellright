1be26231507a9bfa021df0390b372fe585f16f3d
Bartosz Antosik <bartosz.antosik@gmail.com>
2019-07-04 00:13:03 +0200

    Binaries update for Electron version 4.x

--- lib/bin/node-spellchecker/build-macos.sh
+++ lib/bin/node-spellchecker/build-macos.sh
@@ -8,8 +8,11 @@
 # node-gyp rebuild --target=1.8.4 --arch=x64 --dist-url=https://atom.io/download/electron
 # cp build/Release/spellchecker.node ../spellchecker-darwin-1.8.4-x64.node
 
-node-gyp rebuild --target=2.0.7 --arch=x64 --dist-url=https://atom.io/download/electron
-cp build/Release/spellchecker.node ../spellchecker-darwin-2.0.7-x64.node
+# node-gyp rebuild --target=2.0.7 --arch=x64 --dist-url=https://atom.io/download/electron
+# cp build/Release/spellchecker.node ../spellchecker-darwin-2.0.7-x64.node
 
 node-gyp rebuild --target=3.0.10 --arch=x64 --dist-url=https://atom.io/download/electron
 cp build/Release/spellchecker.node ../spellchecker-darwin-3.0.10-x64.node
+
+node-gyp rebuild --target=4.2.5 --arch=x64 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-darwin-4.2.5-x64.node
--- lib/bin/node-spellchecker/build-windows.cmd
+++ lib/bin/node-spellchecker/build-windows.cmd
@@ -10,14 +10,20 @@ REM copy build\Release\spellchecker.node ..\spellchecker-win32-1.8.4-ia32.node
 REM call node-gyp rebuild --target=1.8.4 --arch=x64 --dist-url=https://atom.io/download/electron
 REM copy build\Release\spellchecker.node ..\spellchecker-win32-1.8.4-x64.node
 
-call node-gyp rebuild --target=2.0.7 --arch=ia32 --dist-url=https://atom.io/download/electron
-copy build\Release\spellchecker.node ..\spellchecker-win32-2.0.7-ia32.node
+REM call node-gyp rebuild --target=2.0.7 --arch=ia32 --dist-url=https://atom.io/download/electron
+REM copy build\Release\spellchecker.node ..\spellchecker-win32-2.0.7-ia32.node
 
-call node-gyp rebuild --target=2.0.7 --arch=x64 --dist-url=https://atom.io/download/electron
-copy build\Release\spellchecker.node ..\spellchecker-win32-2.0.7-x64.node
+REM call node-gyp rebuild --target=2.0.7 --arch=x64 --dist-url=https://atom.io/download/electron
+REM copy build\Release\spellchecker.node ..\spellchecker-win32-2.0.7-x64.node
 
 call node-gyp rebuild --target=3.0.10 --arch=ia32 --dist-url=https://atom.io/download/electron
 copy build\Release\spellchecker.node ..\spellchecker-win32-3.0.10-ia32.node
 
 call node-gyp rebuild --target=3.0.10 --arch=x64 --dist-url=https://atom.io/download/electron
 copy build\Release\spellchecker.node ..\spellchecker-win32-3.0.10-x64.node
+
+call node-gyp rebuild --target=4.2.5 --arch=ia32 --dist-url=https://atom.io/download/electron
+copy build\Release\spellchecker.node ..\spellchecker-win32-4.2.5-ia32.node
+
+call node-gyp rebuild --target=4.2.5 --arch=x64 --dist-url=https://atom.io/download/electron
+copy build\Release\spellchecker.node ..\spellchecker-win32-4.2.5-x64.node
