5e393cc7fa969dc87eb46eeb080be460431422d7
Bartosz Antosik <bartosz.antosik@gmail.com>
2020-01-22 23:02:56 +0100

    Building scripts update

--- lib/bin/node-spellchecker/build-linux-rhel.sh
+++ lib/bin/node-spellchecker/build-linux-rhel.sh
@@ -2,14 +2,14 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools
 
-node-gyp rebuild --target=4.2.10 --arch=x64 --dist-url=https://atom.io/download/electron
-cp build/Release/spellchecker.node ../spellchecker-rpm-4.2.10-x64.node
-
-node-gyp rebuild --target=4.2.10 --arch=ia32 --dist-url=https://atom.io/download/electron
-cp build/Release/spellchecker.node ../spellchecker-rpm-4.2.10-ia32.node
-
 node-gyp rebuild --target=6.0.9 --arch=x64 --dist-url=https://atom.io/download/electron
 cp build/Release/spellchecker.node ../spellchecker-rpm-6.0.9-x64.node
 
 node-gyp rebuild --target=6.0.9 --arch=ia32 --dist-url=https://atom.io/download/electron
 cp build/Release/spellchecker.node ../spellchecker-rpm-6.0.9-ia32.node
+
+node-gyp rebuild --target=7.1.7 --arch=x64 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-rpm-7.1.7-x64.node
+
+node-gyp rebuild --target=7.1.7 --arch=ia32 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-rpm-7.1.7-ia32.node
--- lib/bin/node-spellchecker/build-windows.cmd
+++ lib/bin/node-spellchecker/build-windows.cmd
@@ -1,13 +1,13 @@
 rem Requires node-gyp (npm install -g node-gyp) plus CLI developper tools. Before building run 'npm install'.
 
-call node-gyp rebuild --target=4.2.10 --arch=ia32 --dist-url=https://atom.io/download/electron --python=c:\bin\python27\python.exe
-copy build\Release\spellchecker.node ..\spellchecker-win32-4.2.10-ia32.node
-
-call node-gyp rebuild --target=4.2.10 --arch=x64 --dist-url=https://atom.io/download/electron --python=c:\bin\python27\python.exe
-copy build\Release\spellchecker.node ..\spellchecker-win32-4.2.10-x64.node
-
 call node-gyp rebuild --target=6.0.9 --arch=ia32 --dist-url=https://atom.io/download/electron --python=c:\bin\python27\python.exe
 copy build\Release\spellchecker.node ..\spellchecker-win32-6.0.9-ia32.node
 
 call node-gyp rebuild --target=6.0.9 --arch=x64 --dist-url=https://atom.io/download/electron --python=c:\bin\python27\python.exe
 copy build\Release\spellchecker.node ..\spellchecker-win32-6.0.9-x64.node
+
+call node-gyp rebuild --target=7.1.7 --arch=ia32 --dist-url=https://atom.io/download/electron --python=c:\bin\python27\python.exe
+copy build\Release\spellchecker.node ..\spellchecker-win32-7.1.7-ia32.node
+
+call node-gyp rebuild --target=7.1.7 --arch=x64 --dist-url=https://atom.io/download/electron --python=c:\bin\python27\python.exe
+copy build\Release\spellchecker.node ..\spellchecker-win32-7.1.7-x64.node
