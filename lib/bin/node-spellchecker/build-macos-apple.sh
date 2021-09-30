81bb5ef25e288961ec6d09ebc14f7225fa35218e
Nikolaus Kuehn <nikolaus.kuehn@commercetools.com>
2021-09-30 23:43:13 +0200

    fix apple silicon arch name

--- lib/bin/node-spellchecker/build-macos-apple.sh
+++ lib/bin/node-spellchecker/build-macos-apple.sh
@@ -2,14 +2,14 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developer tools
 
-node-gyp rebuild --target=9.1.0 --arch=x64 --dist-url=https://atom.io/download/electron
-cp build/Release/spellchecker.node ../spellchecker-darwin-9.1.0-m1.node
+node-gyp rebuild --target=9.1.0 --arch=arm64 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-darwin-9.1.0-arm64.node
 
-node-gyp rebuild --target=11.1.0 --arch=x64 --dist-url=https://atom.io/download/electron
-cp build/Release/spellchecker.node ../spellchecker-darwin-11.1.0-m1.node
+node-gyp rebuild --target=11.1.0 --arch=arm64 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-darwin-11.1.0-arm64.node
 
-node-gyp rebuild --target=12.0.4 --arch=x64 --dist-url=https://atom.io/download/electron
-cp build/Release/spellchecker.node ../spellchecker-darwin-12.0.4-m1.node
+node-gyp rebuild --target=12.0.4 --arch=arm64 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-darwin-12.0.4-arm64.node
 
-node-gyp rebuild --target=13.1.7 --arch=x64 --dist-url=https://atom.io/download/electron
-cp build/Release/spellchecker.node ../spellchecker-darwin-13.1.7-m1.node
+node-gyp rebuild --target=13.1.7 --arch=arm64 --dist-url=https://atom.io/download/electron
+cp build/Release/spellchecker.node ../spellchecker-darwin-13.1.7-arm64.node
