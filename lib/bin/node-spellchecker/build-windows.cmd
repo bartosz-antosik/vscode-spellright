71ed3bc771123837f9826c15f4fbe7f2f6dec8a4
Nikolaus Kuehn <nikolaus.kuehn@commercetools.com>
2021-10-05 23:41:17 +0200

    electron 6 on win does not build

--- lib/bin/node-spellchecker/build-windows.cmd
+++ lib/bin/node-spellchecker/build-windows.cmd
@@ -30,8 +30,8 @@ copy build\Release\spellchecker.node ..\spellchecker-win32-7.1.7-ia32.node
 call npx node-gyp rebuild --target=7.1.7 --arch=x64 --python=c:\bin\python27\python.exe --dist-url=https://electronjs.org/headers
 copy build\Release\spellchecker.node ..\spellchecker-win32-7.1.7-x64.node
 
-call npx node-gyp rebuild --target=6.0.9 --arch=ia32 --python=c:\bin\python27\python.exe --dist-url=https://electronjs.org/headers
-copy build\Release\spellchecker.node ..\spellchecker-win32-6.0.9-ia32.node
-
-call npx node-gyp rebuild --target=6.0.9 --arch=x64 --python=c:\bin\python27\python.exe --dist-url=https://electronjs.org/headers
-copy build\Release\spellchecker.node ..\spellchecker-win32-6.0.9-x64.node
+rem 6.0.9 has issues building
+rem call npx node-gyp rebuild --target=6.0.9 --arch=ia32 --python=c:\bin\python27\python.exe --dist-url=https://electronjs.org/headers
+rem copy build\Release\spellchecker.node ..\spellchecker-win32-6.0.9-ia32.node
+rem call npx node-gyp rebuild --target=6.0.9 --arch=x64 --python=c:\bin\python27\python.exe --dist-url=https://electronjs.org/headers
+rem copy build\Release\spellchecker.node ..\spellchecker-win32-6.0.9-x64.node
