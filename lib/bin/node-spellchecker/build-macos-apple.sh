2df8c405ba6893be44786673becfe4be5282d19b
Nikolaus Kuehn <nikolaus.kuehn@commercetools.com>
2021-10-05 14:06:07 +0200

    clean up previous versions, fix a filename

--- lib/bin/node-spellchecker/build-macos-apple.sh
+++ lib/bin/node-spellchecker/build-macos-apple.sh
@@ -6,7 +6,7 @@ npx node-gyp rebuild --target=16.5.0 --arch=arm64
 cp build/Release/spellchecker.node ../spellchecker-darwin-16.5.0-arm64.node
 
 npx node-gyp rebuild --target=14.16.0 --arch=arm64
-cp build/Release/spellchecker.node ../spellchecker-darwin-13.1.7-arm64.node
+cp build/Release/spellchecker.node ../spellchecker-darwin-14.16.0-arm64.node
 
 npx node-gyp rebuild --target=12.18.3 --arch=arm64
 cp build/Release/spellchecker.node ../spellchecker-darwin-12.18.3-arm64.node
@@ -16,16 +16,3 @@ cp build/Release/spellchecker.node ../spellchecker-darwin-10.11.0-arm64.node
 
 npx node-gyp rebuild --target=8.9.3 --arch=arm64
 cp build/Release/spellchecker.node ../spellchecker-darwin-8.9.3-arm64.node
-
-
-# npx node-gyp rebuild --target=9.1.0 --arch=arm64 --dist-url=https://atom.io/download/electron
-# cp build/Release/spellchecker.node ../spellchecker-darwin-9.1.0-arm64.node
-
-# npx node-gyp rebuild --target=11.1.0 --arch=arm64 --dist-url=https://atom.io/download/electron
-# cp build/Release/spellchecker.node ../spellchecker-darwin-11.1.0-arm64.node
-
-# npx node-gyp rebuild --target=12.0.4 --arch=arm64 --dist-url=https://atom.io/download/electron
-# cp build/Release/spellchecker.node ../spellchecker-darwin-12.0.4-arm64.node
-
-# npx node-gyp rebuild --target=13.1.7 --arch=arm64 --dist-url=https://atom.io/download/electron
-# cp build/Release/spellchecker.node ../spellchecker-darwin-13.1.7-arm64.node
--- lib/bin/node-spellchecker/build-macos-intel.sh
+++ lib/bin/node-spellchecker/build-macos-intel.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-# requires node-gyp (installed as a package.json dependency) plus CLI developer tools
+# see build-linux-debian.sh for comments
 
 npx node-gyp rebuild --target=16.5.0 --arch=x64
 cp build/Release/spellchecker.node ../spellchecker-darwin-16.5.0-x64.node
 
 npx node-gyp rebuild --target=14.16.0 --arch=x64
-cp build/Release/spellchecker.node ../spellchecker-darwin-13.1.7-x64.node
+cp build/Release/spellchecker.node ../spellchecker-darwin-14.16.0-x64.node
 
 npx node-gyp rebuild --target=12.18.3 --arch=x64
 cp build/Release/spellchecker.node ../spellchecker-darwin-12.18.3-x64.node
@@ -16,18 +15,3 @@ cp build/Release/spellchecker.node ../spellchecker-darwin-10.11.0-x64.node
 
 npx node-gyp rebuild --target=8.9.3 --arch=x64
 cp build/Release/spellchecker.node ../spellchecker-darwin-8.9.3-x64.node
-
-# npx node-gyp rebuild --target=6.0.9 --arch=x64 --dist-url=https://atom.io/download/electron
-# cp build/Release/spellchecker.node ../spellchecker-darwin-6.0.9-x64.node
-
-# npx node-gyp rebuild --target=7.1.7 --arch=x64 --dist-url=https://atom.io/download/electron
-# cp build/Release/spellchecker.node ../spellchecker-darwin-7.1.7-x64.node
-
-# npx node-gyp rebuild --target=9.1.0 --arch=x64 --dist-url=https://atom.io/download/electron
-# cp build/Release/spellchecker.node ../spellchecker-darwin-9.1.0-x64.node
-
-# npx node-gyp rebuild --target=11.1.0 --arch=x64 --dist-url=https://atom.io/download/electron
-# cp build/Release/spellchecker.node ../spellchecker-darwin-11.1.0-x64.node
-
-# npx node-gyp rebuild --target=13.1.7 --arch=x64 --dist-url=https://atom.io/download/electron
-# cp build/Release/spellchecker.node ../spellchecker-darwin-13.1.7-x64.node
