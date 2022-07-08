57e379ca2c370d6d5379237cef54c6852e9e6af0
Borek Bernard <borekb@gmail.com>
2022-07-08 18:55:34 +0200

    Update Apple silicon binaries for VSCode 1.69

--- lib/bin/node-spellchecker/build-macos-apple.sh
+++ lib/bin/node-spellchecker/build-macos-apple.sh
@@ -2,8 +2,8 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developer tools
 
-node-gyp rebuild --target=13.1.7 --arch=arm64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-darwin-13.1.7-arm64.node
-
 node-gyp rebuild --target=17.2.0 --arch=arm64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-darwin-17.2.0-arm64.node
+
+node-gyp rebuild --target=18.3.5 --arch=arm64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-darwin-18.3.5-arm64.node
