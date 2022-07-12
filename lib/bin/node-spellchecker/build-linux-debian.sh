369f42f57bd23eaa7a6765fc68ede8c644ef1dae
Bartosz Antosik <bartosz.antosik@gmail.com>
2022-07-12 23:44:10 +0200

    Initial commit

--- lib/bin/node-spellchecker/build-linux-debian.sh
+++ lib/bin/node-spellchecker/build-linux-debian.sh
@@ -2,14 +2,14 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools
 
-node-gyp rebuild --target=17.2.0 --arch=ia32 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-deb-17.2.0-ia32.node
+npx node-gyp rebuild --target=18.3.5 --arch=x64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-deb-18.3.5-x64.node
 
-node-gyp rebuild --target=17.2.0 --arch=x64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-deb-17.2.0-x64.node
-
-node-gyp rebuild --target=18.3.5 --arch=ia32 --dist-url=https://electronjs.org/headers
+npx node-gyp rebuild --target=18.3.5 --arch=ia32 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-deb-18.3.5-ia32.node
 
-node-gyp rebuild --target=18.3.5 --arch=x64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-deb-18.3.5-x64.node
+npx node-gyp rebuild --target=17.2.0 --arch=x64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-deb-17.2.0-x64.node
+
+npx node-gyp rebuild --target=17.2.0 --arch=ia32 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-deb-17.2.0-ia32.node
