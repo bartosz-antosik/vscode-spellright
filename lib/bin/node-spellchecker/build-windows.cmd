0881a8be6705a4ba73d57f11b2ecece78a7e58e8
Bartosz Antosik <bartosz.antosik@gmail.com>
2022-07-13 01:33:44 +0200

    Let's try latest of 17 builds

--- lib/bin/node-spellchecker/build-windows.cmd
+++ lib/bin/node-spellchecker/build-windows.cmd
@@ -6,8 +6,8 @@ copy build\Release\spellchecker.node ..\spellchecker-win32-18.3.5-x64.node
 call npx node-gyp rebuild --target=18.3.5 --arch=ia32 --dist-url=https://electronjs.org/headers --python=c:\bin\python27\python.exe
 copy build\Release\spellchecker.node ..\spellchecker-win32-18.3.5-ia32.node
 
-call npx node-gyp rebuild --target=17.2.0 --arch=x64 --dist-url=https://electronjs.org/headers --python=c:\bin\python27\python.exe
-copy build\Release\spellchecker.node ..\spellchecker-win32-17.2.0-x64.node
+call npx node-gyp rebuild --target=17.9.1 --arch=x64 --dist-url=https://electronjs.org/headers --python=c:\bin\python27\python.exe
+copy build\Release\spellchecker.node ..\spellchecker-win32-17.9.1-x64.node
 
-call npx node-gyp rebuild --target=17.2.0 --arch=ia32 --dist-url=https://electronjs.org/headers --python=c:\bin\python27\python.exe
-copy build\Release\spellchecker.node ..\spellchecker-win32-17.2.0-ia32.node
+call npx node-gyp rebuild --target=17.9.1 --arch=ia32 --dist-url=https://electronjs.org/headers --python=c:\bin\python27\python.exe
+copy build\Release\spellchecker.node ..\spellchecker-win32-17.9.1-ia32.node
