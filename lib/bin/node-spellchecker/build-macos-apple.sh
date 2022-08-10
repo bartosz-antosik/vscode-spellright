65332e6d74b7926134642c6eaed4409bbc8edc9c
Bartosz Antosik <bartosz.antosik@gmail.com>
2022-08-10 12:32:29 +0200

    New version of Electron

--- lib/bin/node-spellchecker/build-macos-apple.sh
+++ lib/bin/node-spellchecker/build-macos-apple.sh
@@ -2,8 +2,8 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developer tools.
 
+npx node-gyp rebuild --target=19.0.11 --arch=arm64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-macos-19.0.11-arm64.node
+
 npx node-gyp rebuild --target=18.3.5 --arch=arm64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-macos-18.3.5-arm64.node
-
-npx node-gyp rebuild --target=17.9.1 --arch=arm64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-macos-17.9.1-arm64.node
--- lib/bin/node-spellchecker/build-macos-intel.sh
+++ lib/bin/node-spellchecker/build-macos-intel.sh
@@ -2,8 +2,8 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools.
 
+npx node-gyp rebuild --target=19.0.11 --arch=x64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-macos-19.0.11-x64.node
+
 npx node-gyp rebuild --target=18.3.5 --arch=x64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-macos-18.3.5-x64.node
-
-npx node-gyp rebuild --target=17.9.1 --arch=x64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-macos-17.9.1-x64.node
