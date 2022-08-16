b8822abb42c096cda87f81fe8c7b9d5978a993c2
Bartosz Antosik <bartosz.antosik@gmail.com>
2022-08-16 22:37:44 +0200

    Cleaned and remote versions added

--- lib/bin/node-spellchecker/build-linux.sh
+++ lib/bin/node-spellchecker/build-linux.sh
@@ -2,14 +2,20 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools.
 
-npx node-gyp rebuild --target=19.0.11 --arch=x64
+npx node-gyp rebuild --target=19.0.11 --arch=x64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-linux-19.0.11-x64.node
 
-npx node-gyp rebuild --target=19.0.11 --arch=ia32
+npx node-gyp rebuild --target=19.0.11 --arch=ia32 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-linux-19.0.11-ia32.node
 
-npx node-gyp rebuild --target=18.3.5 --arch=x64
+npx node-gyp rebuild --target=18.3.5 --arch=x64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-linux-18.3.5-x64.node
 
-npx node-gyp rebuild --target=18.3.5 --arch=ia32
+npx node-gyp rebuild --target=18.3.5 --arch=ia32 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-linux-18.3.5-ia32.node
+
+npx node-gyp rebuild --arch=x64
+cp build/Release/spellchecker.node ../spellchecker-linux-remote-x64.node
+
+npx node-gyp rebuild --arch=ia32
+cp build/Release/spellchecker.node ../spellchecker-linux-remote-ia32.node
