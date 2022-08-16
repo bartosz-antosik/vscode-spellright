ff42b01b54496117d721877e0b2f658c60831082
Bartosz Antosik <bartosz.antosik@gmail.com>
2022-08-16 17:26:50 +0200

    Let's try to build binaries Electron independent

--- lib/bin/node-spellchecker/build-linux.sh
+++ lib/bin/node-spellchecker/build-linux.sh
@@ -2,14 +2,14 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools.
 
-npx node-gyp rebuild --target=19.0.11 --arch=x64 --dist-url=https://electronjs.org/headers
+npx node-gyp rebuild --target=19.0.11 --arch=x64
 cp build/Release/spellchecker.node ../spellchecker-linux-19.0.11-x64.node
 
-npx node-gyp rebuild --target=19.0.11 --arch=ia32 --dist-url=https://electronjs.org/headers
+npx node-gyp rebuild --target=19.0.11 --arch=ia32
 cp build/Release/spellchecker.node ../spellchecker-linux-19.0.11-ia32.node
 
-npx node-gyp rebuild --target=18.3.5 --arch=x64 --dist-url=https://electronjs.org/headers
+npx node-gyp rebuild --target=18.3.5 --arch=x64
 cp build/Release/spellchecker.node ../spellchecker-linux-18.3.5-x64.node
 
-npx node-gyp rebuild --target=18.3.5 --arch=ia32 --dist-url=https://electronjs.org/headers
+npx node-gyp rebuild --target=18.3.5 --arch=ia32
 cp build/Release/spellchecker.node ../spellchecker-linux-18.3.5-ia32.node
