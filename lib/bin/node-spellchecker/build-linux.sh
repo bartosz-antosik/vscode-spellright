d837810211a380df1be4010983c553c9f69e8cb9
Bartosz Antosik <bartosz.antosik@cloudgpw.pl>
2024-02-01 16:29:44 +0100

    Build scripts updated

--- lib/bin/node-spellchecker/build-linux.sh
+++ lib/bin/node-spellchecker/build-linux.sh
@@ -2,14 +2,14 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools.
 
+npx node-gyp rebuild --target=27.2.0 --arch=x64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-linux-27.2.0-x64.node
+
+npx node-gyp rebuild --target=27.2.0 --arch=ia32 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-linux-27.2.0-ia32.node
+
 npx node-gyp rebuild --target=25.8.0 --arch=x64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-linux-25.8.0-x64.node
 
 npx node-gyp rebuild --target=25.8.0 --arch=ia32 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-linux-25.8.0-ia32.node
-
-npx node-gyp rebuild --target=22.3.8 --arch=x64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-linux-22.3.8-x64.node
-
-npx node-gyp rebuild --target=22.3.8 --arch=ia32 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-linux-22.3.8-ia32.node
--- lib/bin/node-spellchecker/build-macos-apple.sh
+++ lib/bin/node-spellchecker/build-macos-apple.sh
@@ -2,8 +2,8 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developer tools.
 
+npx node-gyp rebuild --target=27.2.0 --arch=arm64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-macos-27.2.0-arm64.node
+
 npx node-gyp rebuild --target=25.8.0 --arch=arm64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-macos-25.8.0-arm64.node
-
-npx node-gyp rebuild --target=22.3.8 --arch=arm64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-macos-22.3.8-arm64.node
