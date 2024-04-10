e587bdb36d4773fd93f5a11a4afcc6ea66954f6a
Bartosz Antosik <bartosz.antosik@cloudgpw.pl>
2024-04-10 11:05:57 +0200

    Build scripts updated for Electron 28.2.10

--- lib/bin/node-spellchecker/build-linux.sh
+++ lib/bin/node-spellchecker/build-linux.sh
@@ -2,14 +2,14 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools.
 
+npx node-gyp rebuild --target=28.2.10 --arch=x64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-linux-28.2.10-x64.node
+
+npx node-gyp rebuild --target=28.2.10 --arch=ia32 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-linux-28.2.10-ia32.node
+
 npx node-gyp rebuild --target=27.2.0 --arch=x64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-linux-27.2.0-x64.node
 
 npx node-gyp rebuild --target=27.2.0 --arch=ia32 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-linux-27.2.0-ia32.node
-
-npx node-gyp rebuild --target=25.8.0 --arch=x64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-linux-25.8.0-x64.node
-
-npx node-gyp rebuild --target=25.8.0 --arch=ia32 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-linux-25.8.0-ia32.node
