d2e75e95e9291bd8de39f6ba36921b2c7cacd013
Bartosz Antosik <bartosz.antosik@cloudgpw.pl>
2024-08-02 02:10:00 +0200

    Build scripts updated for Electron 30

--- lib/bin/node-spellchecker/build-linux.sh
+++ lib/bin/node-spellchecker/build-linux.sh
@@ -2,14 +2,14 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools.
 
+npx node-gyp rebuild --target=30.1.2 --arch=x64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-linux-30.1.2-x64.node
+
+npx node-gyp rebuild --target=30.1.2 --arch=ia32 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-linux-30.1.2-ia32.node
+
 npx node-gyp rebuild --target=29.4.0 --arch=x64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-linux-29.4.0-x64.node
 
 npx node-gyp rebuild --target=29.4.0 --arch=ia32 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-linux-29.4.0-ia32.node
-
-npx node-gyp rebuild --target=28.2.10 --arch=x64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-linux-28.2.10-x64.node
-
-npx node-gyp rebuild --target=28.2.10 --arch=ia32 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-linux-28.2.10-ia32.node
--- lib/bin/node-spellchecker/build-macos-arm.sh
+++ lib/bin/node-spellchecker/build-macos-arm.sh
@@ -2,8 +2,8 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developer tools.
 
+npx node-gyp rebuild --target=30.1.2 --arch=arm64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-macos-30.1.2-arm64.node
+
 npx node-gyp rebuild --target=29.4.0 --arch=arm64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-macos-29.4.0-arm64.node
-
-npx node-gyp rebuild --target=28.2.10 --arch=arm64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-macos-28.2.10-arm64.node
