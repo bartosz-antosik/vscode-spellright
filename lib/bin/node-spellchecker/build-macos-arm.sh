8db0474aa5a39a1baf761bf45f607e712c2f9b37
Bartosz Antosik <bartosz.antosik@cloudgpw.pl>
2024-10-30 19:38:50 +0100

    Binaries for electron 32

--- lib/bin/node-spellchecker/build-macos-arm.sh
+++ lib/bin/node-spellchecker/build-macos-arm.sh
@@ -2,8 +2,8 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developer tools.
 
+npx node-gyp rebuild --target=32.2.1 --arch=arm64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-macos-32.2.1-arm64.node
+
 npx node-gyp rebuild --target=30.1.2 --arch=arm64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-macos-30.1.2-arm64.node
-
-npx node-gyp rebuild --target=29.4.0 --arch=arm64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-macos-29.4.0-arm64.node
