98d589a98a1a1923d3b5ef4733cfba9deb4a8406
Bartosz Antosik <bartosz.antosik@cloudgpw.pl>
2025-12-13 23:04:28 +0100

    Build scripts updated

--- lib/bin/node-spellchecker/build-macos-intel.sh
+++ lib/bin/node-spellchecker/build-macos-intel.sh
@@ -2,8 +2,8 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools.
 
-npx node-gyp rebuild --target=35.5.1 --arch=x64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-macos-35.5.1-x64.node
+npx node-gyp rebuild --target=39.2.3 --arch=x64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-macos-39.2.3-x64.node
 
-npx node-gyp rebuild --target=34.2.0 --arch=x64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-macos-34.2.0-x64.node
+npx node-gyp rebuild --target=37.2.3 --arch=x64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-macos-37.2.3-x64.node
--- lib/bin/node-spellchecker/build-windows.cmd
+++ lib/bin/node-spellchecker/build-windows.cmd
@@ -1,5 +1,17 @@
 rem Requires node-gyp (npm install -g node-gyp) plus CLI developper tools.
 
+call npx node-gyp rebuild --target=39.2.3 --arch=x64 --dist-url=https://electronjs.org/headers --python=c:\bin\python27\python.exe
+copy build\Release\spellchecker.node ..\spellchecker-win32-39.2.3-x64.node
+
+@REM call npx node-gyp rebuild --target=39.2.3 --arch=ia32 --dist-url=https://electronjs.org/headers --python=c:\bin\python27\python.exe
+@REM copy build\Release\spellchecker.node ..\spellchecker-win32-39.2.3-ia32.node
+
+call npx node-gyp rebuild --target=37.2.3 --arch=x64 --dist-url=https://electronjs.org/headers --python=c:\bin\python27\python.exe
+copy build\Release\spellchecker.node ..\spellchecker-win32-37.2.3-x64.node
+
+@REM call npx node-gyp rebuild --target=37.2.3 --arch=ia32 --dist-url=https://electronjs.org/headers --python=c:\bin\python27\python.exe
+@REM copy build\Release\spellchecker.node ..\spellchecker-win32-37.2.3-ia32.node
+
 call npx node-gyp rebuild --target=35.5.1 --arch=x64 --dist-url=https://electronjs.org/headers --python=c:\bin\python27\python.exe
 copy build\Release\spellchecker.node ..\spellchecker-win32-35.5.1-x64.node
 
