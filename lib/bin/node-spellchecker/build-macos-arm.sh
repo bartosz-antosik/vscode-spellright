2f759c085ee279475711929a38b68c51fa90e326
Bartosz Antosik <bartosz.antosik@gmail.com>
2026-06-09 23:35:48 +0200

    Build scripts updated

--- lib/bin/node-spellchecker/build-macos-arm.sh
+++ lib/bin/node-spellchecker/build-macos-arm.sh
@@ -2,8 +2,8 @@
 
 # requires node-gyp (npm install -g node-gyp) plus CLI developer tools.
 
+npx node-gyp rebuild --target=42.4.0 --arch=arm64 --dist-url=https://electronjs.org/headers
+cp build/Release/spellchecker.node ../spellchecker-macos-42.4.0-arm64.node
+
 npx node-gyp rebuild --target=39.2.3 --arch=arm64 --dist-url=https://electronjs.org/headers
 cp build/Release/spellchecker.node ../spellchecker-macos-39.2.3-arm64.node
-
-npx node-gyp rebuild --target=37.2.3 --arch=arm64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-macos-37.2.3-arm64.node
