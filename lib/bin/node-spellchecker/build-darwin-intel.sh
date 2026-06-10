592b7152f2dae7e7456326b85bd1974a896f6423
Bartosz Antosik <bartosz.antosik@gmail.com>
2026-06-10 18:44:06 +0200

    Lets go darwin

--- lib/bin/node-spellchecker/build-darwin-intel.sh
+++ lib/bin/node-spellchecker/build-darwin-intel.sh
@@ -3,7 +3,7 @@
 # requires node-gyp (npm install -g node-gyp) plus CLI developper tools.
 
 npx node-gyp rebuild --target=42.4.0 --arch=x64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-macos-42.4.0-x64.node
+cp build/Release/spellchecker.node ../spellchecker-darwin-42.4.0-x64.node
 
 npx node-gyp rebuild --target=39.2.3 --arch=x64 --dist-url=https://electronjs.org/headers
-cp build/Release/spellchecker.node ../spellchecker-macos-39.2.3-x64.node
+cp build/Release/spellchecker.node ../spellchecker-darwin-39.2.3-x64.node
